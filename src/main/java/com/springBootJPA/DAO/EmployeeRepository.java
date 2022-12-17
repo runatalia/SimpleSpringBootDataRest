cbd97ce73213d11e907b089ff63387704e6807a5
runatalia <voroninana1990@mail.ru>
2022-12-18 01:07:05 +0300

    simple String Boot Data Rest

---
+++ src/main/java/com/springBootJPA/DAO/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springBootJPA.DAO;
+
+import com.springBootJPA.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
+}
